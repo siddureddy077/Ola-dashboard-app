db337b5772a3cc6e8f6c78cdb3d8fd8bf6a41645
siddureddy077 <chinthalasiddu143@gmail.com>
2026-06-21 19:43:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 import streamlit as st
 
 url='OLA_Cleaned_Data.db'
-df=pd.read_csv(OLA_Cleaned_Data.csv)
+df=pd.read_csv(url)
 engine = create_engine("sqlite:///ola.db")
 load_data=df.to_sql('OLA_Cleaned_Data',con=engine,if_exists='replace',index=False)
 
